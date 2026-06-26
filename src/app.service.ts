5ba4a87393a28a6860cb6820fe1fa9cc1fbb34f9
gerabu <aburtogerald@gmail.com>
2026-06-25 21:53:35 -0600

    :egg: Beep Boop

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "These aren't the droids you're looking for";
   }
 }
